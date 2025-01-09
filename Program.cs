f1af9ae5a4fa854449abb2cb65635b45ddad295b
HereisLuvika <kth882500@gmail.com>
2025-01-09 11:51:03 +0900

    0109+

--- Program.cs
+++ Program.cs
@@ -3,6 +3,6 @@
     private static void Main(string[] args)
     {
         Solution sol = new Solution();
-        Console.WriteLine(sol.solution0108(12, 23));
+        Console.WriteLine(sol.solution0109(2, 3)); 
     }
 }
--- Solution.cs
+++ Solution.cs
@@ -1,5 +1,10 @@
 class Solution 
+
 {
+    public int solution0109(int num1, int num2)
+    {
+        return num1 - num2;
+    }
     public int solution0108(int num1, int num2)
     {
         return num1 * num2;
