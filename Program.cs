d03b782f2c49bbcc629688c6bf2b7a8e25bc9883
HereisLuvika <kth882500@gmail.com>
2025-01-09 09:46:54 +0900

    addmultiply

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(solution(82, 80));
+    }
+
+    public static int multiply(int num1, int num2) {
+    int answer = num1 * num2;
+    return answer;
+}
+}
