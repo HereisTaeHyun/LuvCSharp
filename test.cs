fde19bc6063efbc6a67185e2eb98846345adaba3
HereisLuvika <kth882500@gmail.com>
2025-01-13 11:09:15 +0900

    DataType

---
+++ test.cs
@@ -0,0 +1,18 @@
+class Sample
+
+{
+    void DataType()
+    {
+        bool b = true;
+        int i = 50465;
+        long l = 46545646L;
+        float f = 1.45645f;
+        double d = 1.48448d;
+        decimal d2 = 123.45m;
+
+        char c = 'a';
+        string s = "string";
+
+        DateTime dt = new DateTime(2025, 1, 13, 10, 24, 00);
+    }
+}
